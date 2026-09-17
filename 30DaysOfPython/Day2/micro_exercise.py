80a71d82a14c923c78a233c5113f81bb439aa51c
yini-19 <yiyakazahnic@gmail.com>
2026-09-17 14:14:12 +0100

    day2 done

---
+++ 30DaysOfPython/Day2/micro_exercise.py
@@ -0,0 +1,27 @@
+# -----------------
+#     Exercise 1
+# -----------------
+num = "20"
+num = int(num)+10
+print(num)
+
+# -----------------
+#     Exercise 2
+# -----------------
+price = 15.5
+price = int(15.5)
+print(price)
+
+# -----------------
+#     Exercise 3
+# -----------------
+food = "carrot"
+kind = "vegetable"
+qty = 20
+price = 50.0
+is_vegetable = True
+print(type(food))
+print(type(kind))
+print(type(qty))
+print(type(price))
+print(type(is_vegetable))
---
+++ 30DaysOfPython/Day2/practical_task.py
@@ -0,0 +1,5 @@
+name = "chidinma"
+birth_year = 1996
+current_calender_year = "2026"
+age = int(current_calender_year) - birth_year
+print(f"{name} was born in {birth_year} and is {age} years old")
